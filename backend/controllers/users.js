9507a74e6b89a05f0e3887539445b677bd11e1aa
Elya Tumanova <elatumanova@yandex.ru>
2023-09-24 15:45:02 +0300

    sp15

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -131,15 +131,15 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
       console.log(token);
       // вернём токен
-      // res.send({ token })
-      res
-        .cookie('jwt', token, {
-          // token - наш JWT токен, который мы отправляем
-          maxAge: 3600000,
-          httpOnly: true,
-        });
-      res.send({ email: user.email, name: user.name, about: user.about })
-        .end();
+      res.send({ token });
+      // res
+      //   .cookie('jwt', token, {
+      //     // token - наш JWT токен, который мы отправляем
+      //     maxAge: 3600000,
+      //     httpOnly: true,
+      //   });
+      // res.send({ email: user.email, name: user.name, about: user.about })
+      //   .end();
     })
     .catch((err) =>
       // возвращаем ошибку аутентификации
