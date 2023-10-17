2785af801aa883bdf177880d4739de4aba77c3e7
Elya Tumanova <elatumanova@yandex.ru>
2023-10-17 21:27:02 +0300

    pr15

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,7 +4,10 @@ import React from 'react'
 function Card ({card, onCardClick, onCardLike, onCardDelete}) {
 
   const currentUser = React.useContext(CurrentUserContext);
-
+  console.log(card)
+  console.log(card.owner._id)
+  console.log(currentUser)
+  console.log(currentUser._id)
   const isOwn = card.owner._id === currentUser._id;
   const isLiked = card.likes.some(i => i._id === currentUser._id);
 
