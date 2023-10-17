7a05319877abf3fa0e7b71e90c1612fdce5b9bce
Elya Tumanova <elatumanova@yandex.ru>
2023-10-17 19:39:16 +0300

    pr 15

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -6,10 +6,9 @@ function Card ({card, onCardClick, onCardLike, onCardDelete}) {
   const currentUser = React.useContext(CurrentUserContext);
 
   console.log (card);
-  console.log (card.data.owner);
   console.log (card.owner);
 
-  const isOwn = card.data.owner === currentUser._id;
+  const isOwn = card.owner === currentUser._id;
   const isLiked = card.data.likes.some(i => i._id === currentUser._id);
 
   const cardLikeButtonClassName = ( 
